c0d421cb0a8a454cc7ef4c1098f9cca2b6a553f4
muzzlol <muzxmmilkhxn@gmail.com>
2025-01-11 22:58:07 +0300

    idk

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/muzzlol/gRPCwgolangez/proto"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedCoffeeShopServer
+	menuItems   []*pb.Item
+	menuUpdates chan *pb.Item
+}
+
+func (s *server) GetMenu(req *pb.MenuRequest, stream pb.CoffeeShop_GetMenuServer) error {
+
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	pb.RegisterCoffeeShopServer(s, &server{})
+}
