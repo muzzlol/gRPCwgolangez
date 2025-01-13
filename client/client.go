159585a1a8f1d72e2d7540e3a189424c68820dbb
muzzlol <muzxmmilkhxn@gmail.com>
2025-01-13 16:22:33 +0300

    simulated streaming menu items and made reciept generator. timed everything

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 	"time"
 
@@ -11,27 +12,59 @@ import (
 )
 
 func main() {
-	conn, err := grpc.NewClient("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	start := time.Now()
+
+	// Connection setup timing
+	connStart := time.Now()
+	conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
 	defer conn.Close()
+	log.Printf("Connection setup took: %v", time.Since(connStart))
 
 	client := pb.NewCoffeeShopClient(conn)
 
+	// Menu streaming timing
+	menuStart := time.Now()
 	stream, err := client.GetMenu(context.Background(), &pb.MenuRequest{})
 	if err != nil {
 		log.Fatalf("failed to get menu: %v", err)
 	}
 
-	for {
-		start := time.Now()
-		menu, err := stream.Recv()
-		if err != nil {
-			log.Fatalf("failed to receive menu: %v", err)
+	done := make(chan bool)
+
+	var items []*pb.Item
+	go func() {
+		for {
+			itemStart := time.Now()
+			menu, err := stream.Recv()
+
+			if err == io.EOF {
+				done <- true
+				break
+			}
+
+			if err != nil {
+				log.Fatalf("failed to receive menu: %v", err)
+			}
+
+			items = append(items, menu.Items...)
+			log.Printf("Received menu item in: %v", time.Since(itemStart))
 		}
-		duration := time.Since(start)
-		log.Printf("menu: %v", menu)
-		log.Printf("Time taken to receive menu: %v", duration)
+	}()
+	<-done
+	log.Printf("Menu streaming took: %v", time.Since(menuStart))
+
+	// Order placement timing
+	orderStart := time.Now()
+	reciept, err := client.PlaceOrder(context.Background(), &pb.Order{Items: items})
+	if err != nil {
+		log.Fatalf("failed to place order: %v", err)
 	}
+	log.Printf("Order placement took: %v", time.Since(orderStart))
+	log.Printf("Reciept: %v", reciept)
+
+	// Total time
+	log.Printf("Total operation took: %v", time.Since(start))
 }
--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
@@ -10,32 +11,55 @@ import (
 
 type server struct {
 	pb.UnimplementedCoffeeShopServer
-	menuItems   []*pb.Item
-	menuUpdates chan *pb.Item
 }
 
 func (s *server) GetMenu(req *pb.MenuRequest, stream pb.CoffeeShop_GetMenuServer) error {
-	menu := &pb.Menu{
-		Items: s.menuItems,
+	items := []*pb.Item{
+		{Id: 1, Name: "Espresso", Price: 2.5},
+		{Id: 2, Name: "Latte", Price: 3.0},
+		{Id: 3, Name: "Cappuccino", Price: 3.0},
+		{Id: 4, Name: "Mocha", Price: 3.5},
+		{Id: 5, Name: "Americano", Price: 2.0},
+		{Id: 6, Name: "Macchiato", Price: 3.0},
+		{Id: 7, Name: "Flat White", Price: 3.5},
+		{Id: 8, Name: "Long Black", Price: 2.5},
+		{Id: 9, Name: "Affogato", Price: 4.0},
+		{Id: 10, Name: "Irish Coffee", Price: 5.0},
+		{Id: 11, Name: "Cheesecake", Price: 3.0},
+		{Id: 12, Name: "Brownie", Price: 2.5},
+		{Id: 13, Name: "Croissant", Price: 2.0},
+		{Id: 14, Name: "Muffin", Price: 2.0},
+		{Id: 15, Name: "Cookie", Price: 1.5},
+		{Id: 16, Name: "Sandwich", Price: 4.0},
+		{Id: 17, Name: "Salad", Price: 5.0},
+		{Id: 18, Name: "Pasta", Price: 6.0},
+		{Id: 19, Name: "Pizza", Price: 7.0},
+		{Id: 20, Name: "Burger", Price: 5.0},
+	}
+	// simulaitng streaming the menu by sending each item one by one
+	for i := range items {
+		stream.Send(&pb.Menu{Items: []*pb.Item{items[i]}})
 	}
 
-	if err := stream.Send(menu); err != nil {
-		return err
-	}
+	return nil
+}
 
-	for {
-		select {
-		case <-stream.Context().Done():
-			return nil
-		case newItem := <-s.menuUpdates:
-			s.menuItems = append(s.menuItems, newItem)
-			// sending de whole menu again
-			if err := stream.Send(&pb.Menu{Items: s.menuItems}); err != nil {
-				return err
-			}
+func (s *server) PlaceOrder(ctx context.Context, order *pb.Order) (*pb.Reciept, error) {
+	var total float32
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	default:
+		for _, item := range order.Items {
+			total += item.Price
 		}
+		return &pb.Reciept{Total: total}, nil
 	}
 }
+
+// func (s *server) GetOrderStatus(ctx context.Context, reciept *pb.Reciept) (pb.OrderStatus, error) {
+
+// }
 
 func main() {
 	lis, err := net.Listen("tcp", ":8080")
@@ -43,21 +67,8 @@ func main() {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
-	srv := &server{
-		// menuItems is a slice of pointers to Item structs
-		menuItems: []*pb.Item{
-			{Name: "Espresso", Price: 2.50},
-			{Name: "Latte", Price: 3.00},
-			{Name: "Cappuccino", Price: 3.00},
-			{Name: "Mocha", Price: 3.50},
-			{Name: "Americano", Price: 2.00},
-		},
-		// menuUpdates is a channel that receives pointers to Item structs in order to update the menu
-		menuUpdates: make(chan *pb.Item),
-	}
-
 	s := grpc.NewServer()
-	pb.RegisterCoffeeShopServer(s, srv)
+	pb.RegisterCoffeeShopServer(s, &server{})
 
 	log.Printf("Server listening on %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
